d74ea4f1731b6915ef4a795d6e36037b7d3f394f
Apoorve Kumar verma <vapoorve@gmail.com>
2018-02-28 16:58:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get(web, function (req, res) {
-  res.send('server is working...',web);
-});
 
 
 app.get('/Avi', function (req, res) {
