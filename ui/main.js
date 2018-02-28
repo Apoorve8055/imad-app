f22f8de05d81fb5500591f97445f614157b9685b
Apoorve Kumar verma <vapoorve@gmail.com>
2018-02-28 16:42:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-alert("you have been");
+console.log('Loaded!');
+var con = document.getElementById('data');
+con.innerHTML = "hi there this is js";
+var pic = document.getElementById('pic');
+var marginleft = 0;
+function moveRight()
+{
+	marginleft = marginleft + 5;
+	pic.style.marginLeft = marginleft + "px";
+};
+pic.onclick = function(){
+//	pic.style.height = "100px";
+var interval = setInterval(moveRight,50);
+};
