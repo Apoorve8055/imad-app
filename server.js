ec3b36c7330ff388244f0d13142538ec4d5a1d77
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-02 21:29:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config =
     port:'5432',
     password:process.env.DB_PASSWORD,
 };
-
+/*
 var pool = new Pool(config);
 app.get('/test-db',function(req,res)
 {
@@ -26,7 +26,7 @@ app.get('/test-db',function(req,res)
       }
   });
 });
-
+*/
 
 var app = express();
 app.use(morgan('combined'));
