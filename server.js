ad04b40d0e8b7670ab8d34335e234a38143cdbd4
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-03 18:27:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,27 +2,17 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var con ={
-    user:'vapoorve',
-    database:'vapoorve',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD,
+
+var config = {
+    user: 'vapoorve',
+    database: 'vapoorve',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
 app.use(morgan('combined'));
-
-var pool = new Pool(con);
-
-app.get('/test-db',function(req,res)
-{   
-    pool.query('SELECT * FROM test', function (req, result) {
-      result.send("test");
-        
-    });
-});
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -38,6 +28,19 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+////////////////////////////////////////////
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM test', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+   });
 });
 
 
