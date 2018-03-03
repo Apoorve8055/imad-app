e213d87fd5bff2d3e2b02ec29cddfb13a0f21fd7
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-04 00:42:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'coco98',
+    database: 'coco98',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
+var app = express();
+app.use(morgan('combined'));
+
 
 var articles = {
     'article-one': {
