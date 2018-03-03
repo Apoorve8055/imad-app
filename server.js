ce0dd297d9e836b2a172f4312ac7e18cd3d93454
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-03 17:38:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(morgan('combined'));
 
 var pool = new Pool(con);
 app.get('/test-db',function(req,res)
-{   var test = pool.query('SELECT * FROM test', function (req, result){
+{   var test = pool.query('SELECT * FROM test', function (req, res){
        res.send("yooooooooooooooo");
-       //res.send(JSON,stringify(result));
+       res.send(JSON,stringify(res);
        //res.write(JSON.stringify(result));
            /*
        if (err) {          
