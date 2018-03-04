0ba4c9de5fdbbbd6e15ce9a0c356929dfa9d6e1d
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-04 10:34:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
 var config = {
        user: 'vapoorve',
     database: 'vapoorve',
@@ -11,41 +10,12 @@ var config = {
     password: process.env.DB_PASSWORD
 
 };
-
 var app = express();
 app.use(morgan('combined'));
+////////////////////////////////////////////////////////////////////
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT * FROM test', function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-var counter = 0;
-app.get('/counter', function (req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
-  // Get the name from the request
-  var name = req.query.name;
-  
-  names.push(name);
-  // JSON: Javascript Object Notation
-  res.send(JSON.stringify("xxx"));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -59,6 +29,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+///////////////////////////////////////////////////////////////////
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
