9e8c117621688d7032c0633c4ca68dc325f3187e
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-04 10:29:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,80 +15,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-
-var articles = {
-    'article-one': {
-      title: 'Article One | Tanmai Gopal',
-      heading: 'Article One',
-      date: 'Sep 5, 2016',
-      content: `
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-          </p>
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-          </p>
-          <p>
-              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-          </p>`
-    },
-    'article-two': {
-      title: 'Article Two | Tanmai Gopal',
-      heading: 'Article Two',
-      date: 'Sep 10, 2016',
-      content: `
-          <p>
-              This is the content for my second article.
-          </p>`
-    },
-    'article-three': {
-      title: 'Article Three | Tanmai Gopal',
-      heading: 'Article Three',
-      date: 'Sep 15, 2016',
-      content: `
-          <p>
-              This is the content for my third article.
-          </p>`
-    }
-};
-
-function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    
-    var htmlTemplate = `
-    <html>
-      <head>
-          <title>
-              ${title}
-          </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
-      </head> 
-      <body>
-          <div class="container">
-              <div>
-                  <a href="/">Home</a>
-              </div>
-              <hr/>
-              <h3>
-                  ${heading}
-              </h3>
-              <div>
-                  ${date}
-              </div>
-              <div>
-                ${content}
-              </div>
-          </div>
-      </body>
-    </html>
-    `;
-    return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -122,7 +48,7 @@ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
   res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName', function (req, res) {
+app.get('/articles', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
   
