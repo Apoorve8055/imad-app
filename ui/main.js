1ef7e790d56a7540eaec3bce31167c5d4a004f78
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-07 12:20:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,6 @@
 // Submit username/password to login
 var submit = document.getElementById('sub');
-submit.onclick = function () {
-  
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-              alert('Logged in successfully');
-          } else if (request.status === 403) {
-              alert('Username/password is incorrect');
-          } else if (request.status === 500) {
-              alert('Something went wrong on the server');
-          }
-      }  
-      // Not done yet
-    };
-    
+
 submit.onclick = function () {
   
     // Create a request object
