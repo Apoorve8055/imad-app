da804c0d43f58b5f0cc9185870ad0f1078bac9ca
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-07 13:06:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,7 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://vapoorve.imad.hasura-app.io/login', true);
+    request.setRequestHeader('content-type','application/json');
     request.send(JSON.stringify({username: username, pass: password}));  
 
 };
