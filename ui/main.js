977abddeb5973a28f2258c45dc9a7c4a09bb9a81
Apoorve Kumar verma <vapoorve@gmail.com>
2018-03-07 12:20:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,25 @@
 // Submit username/password to login
 var submit = document.getElementById('sub');
-
 submit.onclick = function () {
-  alert("yoooooooooo");
+  
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              alert('Logged in successfully');
+          } else if (request.status === 403) {
+              alert('Username/password is incorrect');
+          } else if (request.status === 500) {
+              alert('Something went wrong on the server');
+          }
+      }  
+      // Not done yet
     };
-/*    
+    
 submit.onclick = function () {
   
     // Create a request object
@@ -33,4 +48,4 @@ submit.onclick = function () {
     request.open('POST', 'http://coco98.imad.hasura-app.io/login', true);
     request.send(JSON.strinfigy({username: username, password: password}));  
 
-};*/
+};
